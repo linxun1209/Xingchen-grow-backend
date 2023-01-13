09ecd73d5c6d765ca95f61039beb33edfac12376
linxun1209 <3056726345@qq.com>
2023-01-13 17:24:16 +0800

    项目初始化(父工程和基础工程)

---
+++ xingchen-grow/src/test/java/com/xingchen/grow/GrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xingchen.grow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
